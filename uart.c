6eb802f2d2b5c273b54a5bfe63bf17062d042eda
AnMiKi <andi_killinger@yahoo.de>
2025-07-20 19:21:02 +0200

    small bugfix in uart transmit

--- uart.c
+++ uart.c
@@ -7,7 +7,7 @@ void uart_init(void){
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);  // 8-bit data, 1 stop, no parity
 }
 
-void uart_transmit(void){
+void uart_transmit(char data){
     while (!(UCSR0A & (1 << UDRE0)));  // Wait for buffer to be empty
     UDR0 = data;
 }
